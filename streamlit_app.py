c998fe6eedbd447ccc11404c8bf71b75982a1375
Abdulgituse <148359146+Abdulgituse@users.noreply.github.com>
2023-10-18 20:26:33 +0530

    Update streamlit_app.py
    Added emoji to items
    Add one more food item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New Health Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry oatmeal')
-streamlit.text('kale, spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 kale, spinach  & Rocket smoothie')
+streamlit.text('🐔 Hard-Boiled free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+ 
